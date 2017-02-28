724044a4de0a155db9e82584a9f2a889989dc3ff
wjc <wjc@huishu.com.cn>
2017-02-28 09:00:31 +0800

    添加测试

--- src/test/java/com/sparrow/MtimeMovieTest.java
+++ src/test/java/com/sparrow/MtimeMovieTest.java
@@ -2,8 +2,9 @@ package com.sparrow;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 import org.apache.commons.lang3.StringUtils;
 import org.jsoup.Jsoup;
@@ -13,9 +14,7 @@ import org.jsoup.select.Elements;
 import org.junit.Test;
 
 import com.sparrow.base.BaseTests;
-import com.sparrow.crawler.entity.Book;
 import com.sparrow.crawler.entity.mtime.Movie;
-import com.sparrow.util.DateUtils;
 import com.sparrow.util.FileUtils;
 import com.sparrow.util.HttpUtils;
 
@@ -27,24 +26,6 @@ import com.sparrow.util.HttpUtils;
  * @date 2017年2月18日
  */
 public class MtimeMovieTest extends BaseTests {
-
-	@Test
-	public void testBatchAddMovieUrl(){
-		Date currentDate = DateUtils.currentDate();
-		
-		Book book = new Book();
-		book.setBookId("000002");
-		book.setBookName("Effective Java");
-		book.setBookPrice(78D);
-		book.setCreateDate(currentDate);
-		book.setCreateUser("wjc");
-		book.setUpdateDate(currentDate);
-		book.setUpdateUser("wjc");
-		
-		String url = "/mtime/batchAddMovieUrl";
-		String response = performAndGetResponse(url, book);
-		logger.info("添加图书，图书信息：{}，执行结果：{}", book, response);
-	}
 	
 	@Test
 	public void testFetchMtimeMovieInfo(){
@@ -91,9 +72,22 @@ public class MtimeMovieTest extends BaseTests {
 		list.addAll(hotMovieList);
 		list.addAll(topHotMovieList);
 		
-		String url = "/mtime/batchAddMovie";
-		String response = performAndGetResponse(url, list);
-		logger.info("批量添加电影信息，电影信息：{}，执行结果：{}", list, response);
+		List<Movie> distinctMovieList = getDistinctMovieList(list);
+		
+//		String url = "/mtime/batchAddMovie";
+//		String response = performAndGetResponse(url, distinctMovieList);
+//		logger.info("批量添加电影信息，电影信息：{}，执行结果：{}", distinctMovieList, response);
+	}
+	
+	private List<Movie> getDistinctMovieList(List<Movie> list){
+		List<Movie> result = new ArrayList<Movie>();
+		Map<String, Movie> map = new TreeMap<String, Movie>();
+		for(Movie item : list){
+			map.put(item.getMovieId(), item);
+		}
+		result.addAll(map.values());
+		
+		return result;
 	}
 	
 	private List<Movie> getGlobalNewMovieList(Document doc){
@@ -159,22 +153,25 @@ public class MtimeMovieTest extends BaseTests {
 		logger.info("热门榜单");
 		Elements elements = doc.select("div[id=divToplistHot] > dl > dd > ul > li");
 		for (Element element : elements) {
+			System.out.println(element.outerHtml());
 			String movieId = element.select("span").attr("objid");
 			if(StringUtils.isNotEmpty(movieId)){
 				String movieUrl = element.select("h3 > a").attr("href");
-				String title = element.select("h3 > a").text();
-				if(title.indexOf("(") != -1){
-					title = title.substring(0, title.indexOf("("));
+				if(!movieUrl.startsWith("http://people")){
+					String title = element.select("h3 > a").text();
+					if(title.indexOf("(") != -1){
+						title = title.substring(0, title.indexOf("("));
+					}
+					//logger.info("movieId: {}， title: {}, movieUrl: {}", movieId, title, movieUrl);
+					
+					Movie movie = new Movie();
+					movie.setMovieId("mtime"+movieId);
+					movie.setTitle(title);
+					movie.setUrl(movieUrl+"trailer.html");
+					movie.setIsCrawler(0);
+					movie.setWebsite("时光网");
+					list.add(movie);
 				}
-				logger.info("movieId: {}， title: {}, movieUrl: {}", movieId, title, movieUrl);
-				
-				Movie movie = new Movie();
-				movie.setMovieId("mtime"+movieId);
-				movie.setTitle(title);
-				movie.setUrl(movieUrl+"trailer.html");
-				movie.setIsCrawler(0);
-				movie.setWebsite("时光网");
-				list.add(movie);
 			}
 		}
 		
