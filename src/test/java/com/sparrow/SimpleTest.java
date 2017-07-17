cef98196f0a8a7ac2bfa9cfbb6966a890eded404
wjc <wjc@huishu.com.cn>
2017-07-17 19:06:55 +0800

    提交单元测试

--- src/test/java/com/sparrow/SimpleTest.java
+++ src/test/java/com/sparrow/SimpleTest.java
@@ -3,6 +3,7 @@ package com.sparrow;
 import java.io.IOException;
 
 import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
@@ -23,7 +24,7 @@ public class SimpleTest {
 	protected final Logger logger = LoggerFactory.getLogger(getClass());
 
 	@Test
-	public void testHttpClient() {
+	public void testHttpClientWithPost() {
 		CloseableHttpClient client = null;
 		try {
 			client = HttpClients.createDefault();
@@ -52,6 +53,73 @@ public class SimpleTest {
 				e.printStackTrace();
 			}
 		}
+	}
+	
+	@Test
+	public void testHttpClientWithGet() {
+		CloseableHttpClient client = null;
+		try {
+			client = HttpClients.createDefault();
+//			String url = "http://cxcyfw.com/contents/23/4184.html";
+			String url = "http://www.cxcyfw.com/contents/4/5036.html";
+			HttpGet getMethod = new HttpGet(url);
+			logger.info("postMethod: {}", getMethod);
+
+			HttpResponse response = client.execute(getMethod);
+			int statusCode = response.getStatusLine().getStatusCode();
+			logger.info("StatusCode: {}", statusCode);
+			logger.info("response: {}", response);
+			if (statusCode == 200) {
+				String entity = EntityUtils.toString(response.getEntity(), SysConst.ENCODING_UTF_8);
+//				logger.info("response result: {}", entity);
+				String selectorName = "class";
+				String selectorValue = "TRS_Editor";
+				String textContent = getTextFromHTML(entity, selectorName, selectorValue);
+				logger.info("textContent: {}", textContent);
+				System.out.println(textContent);
+				EntityUtils.consume(response.getEntity());
+			} else {
+				EntityUtils.consume(response.getEntity());
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (client != null)
+					client.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	/**
+	 * 
+	 * <p>Description: 从html内容中获取去除html标签以后的文本内容</p>
+	 * @param htmlStr
+	 * @return
+	 * @author wjc
+	 * @date 2017年5月4日
+	 */
+	public String getTextFromHTML(String htmlStr, String selectorKey, 
+			String selectorValue) {
+		if(htmlStr == null || htmlStr.trim().equals("")){
+			return "";
+		}
+		Document doc = Jsoup.parse(htmlStr);
+		String text = doc.getElementsByAttributeValue(selectorKey, selectorValue).text();
+		// remove extra white space
+		StringBuilder builder = new StringBuilder(text);
+		int index = 0;
+		while (builder.length() > index) {
+			char tempChar = builder.charAt(index);
+			if (Character.isSpaceChar(tempChar) || Character.isWhitespace(tempChar)) {
+				builder.setCharAt(index, ' ');
+			}
+			index++;
+		}
+		text = builder.toString().replaceAll(" +", " ").trim();
+		return text;
 	}
 
 	@Test
@@ -94,6 +162,13 @@ public class SimpleTest {
 	@Test
 	public void testHttpClient2(){
 		String url = "http://esf.hf.fang.com/";
+		String result = HttpUtils.getRawHtml(url);
+		logger.info("result: {}", result);
+	}
+	
+	@Test
+	public void testHttpClient3(){
+		String url = "http://cxcyfw.com/contents/23/4184.html";
 		String result = HttpUtils.getRawHtml(url);
 		logger.info("result: {}", result);
 	}
